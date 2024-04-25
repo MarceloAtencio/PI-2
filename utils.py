f39c870c1ea8d0a85d7253844303bf4136ff1c94
Marcelo Atencio <136397718+MarceloAtencio@users.noreply.github.com>
2024-04-25 11:05:21 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,38 @@
-##prueba
+import pandas as pd
+
+
+def data_summ(df, title=None):
+    '''
+    Function to provide detailed information about the dtype, null values,
+    and outliers for each column in a DataFrame.
+
+    Parameters:
+    - df (pd.DataFrame): The DataFrame for which information is to be generated.
+    - title (str, optional): Title to be used in the summary. If None, the title will be omitted.
+
+    Returns:
+    - df_info (pd.DataFrame): A DataFrame containing information about each column,
+                              including data type, non-missing quantity, percentage of
+                              missing values, missing quantity, and information about outliers.
+    '''
+    info_dict = {"Column": [], "Data_type": [], "No_miss_Qty": [], "%Missing": [], "Missing_Qty": []}
+
+    for column in df.columns:
+        info_dict["Column"].append(column)
+        info_dict["Data_type"].append(df[column].apply(type).unique())
+        info_dict["No_miss_Qty"].append(df[column].count())
+        info_dict["%Missing"].append(round(df[column].isnull().sum() * 100 / len(df), 2))
+        info_dict['Missing_Qty'].append(df[column].isnull().sum())
+
+  
+    df_info = pd.DataFrame(info_dict)
+
+    if title:
+        print(f"{title} Summary")
+        print("\nTotal rows: ", len(df))
+        print("\nTotal full null rows: ", df.isna().all(axis=1).sum())
+
+    print(df_info.to_string(index=False))
+    print("=====================================")
+
+    return df_info
